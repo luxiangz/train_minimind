f76b05966093d2e7d48fbe0c4fea408fcddeb56f
MorleyOlsen <1597493790@qq.com>
2026-08-20 09:19:10 +0800

    添加mlp和block

--- model/model_minimind.py
+++ model/model_minimind.py
@@ -134,3 +134,37 @@ class Attention(torch.nn.Module):
         output = output.transpose(1, 2).reshape(bsz, seq_len, -1)
         output = self.resid_dropout(self.wo(output))
         return output, past_kv
+
+class FeedForward(torch.nn.Module):
+    def __init__(self, config: MiniMindConfig, intermediate_size: int = None):
+        super().__init__()
+        intermediate_size = intermediate_size or config.intermediate_size
+        self.gate_proj = torch.nn.Linear(config.hidden_size, intermediate_size, bias=False)
+        self.up_proj = torch.nn.Linear(config.hidden_size, intermediate_size, bias=False)
+        self.down_proj = torch.nn.Linear(intermediate_size, config.hidden_size, bias=False)
+
+    def forward(self, x):
+        return self.down_proj(torch.nn.functional.silu(self.gate_proj(x)) * self.up_proj(x))
+
+# TODO
+class MOEFeedForward(torch.nn.Module):
+    def __init__(self, config: MiniMindConfig, intermediate_size: int = None):
+        super().__init__()
+
+class MinimindBlock(torch.nn.Module):
+    def __init__(self, config: MiniMindConfig):
+        super().__init__()
+        self.self_attn = Attention(config)
+        self.mlp = FeedForward(config) if not config.use_moe else MOEFeedForward(config)
+        self.input_layernorm = RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+        self.post_attention_layernorm = RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+
+    def forward(self, hidden_states, position_embeddings, past_key_value=None, use_cache=False, attention_mask=None):
+        residual = hidden_states
+        hidden_states, present_key_value = self.self_attn(
+            self.input_layernorm(hidden_states), position_embeddings,
+            past_key_value, use_cache, attention_mask
+        )
+        hidden_states += residual
+        hidden_states = hidden_states + self.mlp(self.post_attention_layernorm(hidden_states))
+        return hidden_states, present_key_value
